d7588be4893dc41ae5dcb7316e2e369de6deeb4f
alexandramunoz <amunoz@ufl.edu>
2016-02-17 23:02:11 -0500

    Initial Commit

---
+++ Project 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 1
+//
+//  Created by Alexandra Munoz on 2/17/16.
+//  Copyright © 2016 Alexandra Munoz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
